4ec6fe956c27a643c945b4b1f9fdf1236694e93f
Lukas Slouka <lukslouka@gmail.com>
2020-05-03 18:29:27 +0200

    fix(serverless-calendar)

--- serverless-calendar/api/main.py
+++ serverless-calendar/api/main.py
@@ -1,10 +1,5 @@
-import datetime
 import logging
-import os
-import typing
-import uuid
 
-from dateutil.parser import parse
 from firebase_admin import (
     firestore,
     initialize_app,
@@ -14,12 +9,11 @@ from flask import (
     Request,
     request,
 )
-from flask_cors import CORS
+# from flask_cors import CORS
 from google.cloud import (
     logging as cloud_logging,
     # tasks,
 )
-from google.protobuf import timestamp_pb2
 from werkzeug.datastructures import Headers
 
 log_client = cloud_logging.Client()
@@ -30,7 +24,7 @@ cloud_logger.addHandler(log_handler)
 
 # Flask setup
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 # Cloud tasks setup
 # client = tasks.CloudTasksClient()
@@ -45,17 +39,18 @@ fire_app = initialize_app()
 db = firestore.client(fire_app)
 
 
-def bad_request(err: str) -> typing.Tuple[dict, int]:
-    """Processes bad request on API
-
-    :param err: string message
-    :return: error response and status code
-    """
-    logging.warning({
-        "message": "Failed to process API request",
-        "error": err
-    })
-    return dict(error=err), 503
+#
+# def bad_request(err: str) -> typing.Tuple[dict, int]:
+#     """Processes bad request on API
+#
+#     :param err: string message
+#     :return: error response and status code
+#     """
+#     logging.warning({
+#         "message": "Failed to process API request",
+#         "error": err
+#     })
+#     return dict(error=err), 503
 
 
 @app.route('/', methods=['GET'])
@@ -75,95 +70,96 @@ def get_calendar_events():
     return response, 200
 
 
-@app.route('/', methods=['POST'])
-def create_calendar_event():
-    """Creates new calendar event
-
-    Accepts following json request attributes:
-    - message: string message of the calendar event
-    - timestamp: RFC 3339 timestamp (ISO format) of when the event is happening
-    - timedelta: number of seconds that must pass until the event is triggered
-    - repeat: number of times the event will repeat after the set timedelta
-
-    timestamp and timedelta are mutually exclusive
-    periodic is used only for timedelta events
-
-    :return: newly created calendar event
-    """
-
-    def check_timestamp(value):
-        if value is None:
-            return None
-        if not isinstance(value, str):
-            raise ValueError('Must be a string')
-        return parse(value)
-
-    request_json = request.get_json(silent=True)
-    logging.info({
-        "method": request.method,
-        "endpoint": request.endpoint,
-        "request": request_json
-    })
-
-    message = request.get('message', "Empty Message")
-
-    try:
-        timestamp = check_timestamp(request_json.get('timestamp'))
-        if timestamp <= datetime.datetime.now():
-            return bad_request("Invalid timestamp (must be a future timestamp)")
-    except Exception as ex:
-        return bad_request("Invalid timestamp ({})".format(str(ex)))
-
-    timedelta = request.get('timedelta')
-    if timedelta is not None and not isinstance(timedelta, int):
-        return bad_request("Invalid timedelta (Must be an integer)")
-
-    repeat = request.get('repeat')
-    if repeat is not None and not isinstance(repeat, int):
-        return bad_request("Invalid repeat (Must be an integer)")
-
-    if timestamp and timedelta:
-        return bad_request("timestamp and timedelta are mutually exclusive")
-
-    if not timedelta and not timedelta:
-        return bad_request("one of timestamp and timedelta must be set")
-
-    # create new task
-    task_id = str(uuid.uuid4())
-    schedule_time = datetime.datetime.now() + datetime.timedelta(seconds=timedelta)
-    proto_timestamp = timestamp_pb2.Timestamp()
-    proto_timestamp.FromDateTime(schedule_time)
-    task = {
-        'http_request': {
-            'http_method': 'POST',
-            'url': os.getenv("EVENT_CALLBACK_URL"),
-            'oidc_token': {
-                'service_account_email': os.getenv('SERVICE_ACCOUNT_EMAIL')
-            },
-            'body': {
-                'message': message,
-                'timedelta': timedelta,
-                'id': task_id,
-                'repeat': repeat
-            },
-            'name': task_id
-        }
-    }
-    task_doc = {
-        'processed': False,
-        'schedule_time': schedule_time.isoformat(),
-        **task,
-    }
-    db.collection('events').document(task_id).set(task_doc)
-    # response = client.create_task(
-    #     parent=task_queue,
-    #     task={
-    #         'schedule_time': proto_timestamp,
-    #         **task,
-    #     }
-    # )
-    # print(response)
-    return task_doc, 201
+#
+# @app.route('/', methods=['POST'])
+# def create_calendar_event():
+#     """Creates new calendar event
+#
+#     Accepts following json request attributes:
+#     - message: string message of the calendar event
+#     - timestamp: RFC 3339 timestamp (ISO format) of when the event is happening
+#     - timedelta: number of seconds that must pass until the event is triggered
+#     - repeat: number of times the event will repeat after the set timedelta
+#
+#     timestamp and timedelta are mutually exclusive
+#     periodic is used only for timedelta events
+#
+#     :return: newly created calendar event
+#     """
+#
+#     def check_timestamp(value):
+#         if value is None:
+#             return None
+#         if not isinstance(value, str):
+#             raise ValueError('Must be a string')
+#         return parse(value)
+#
+#     request_json = request.get_json(silent=True)
+#     logging.info({
+#         "method": request.method,
+#         "endpoint": request.endpoint,
+#         "request": request_json
+#     })
+#
+#     message = request.get('message', "Empty Message")
+#
+#     try:
+#         timestamp = check_timestamp(request_json.get('timestamp'))
+#         if timestamp <= datetime.datetime.now():
+#             return bad_request("Invalid timestamp (must be a future timestamp)")
+#     except Exception as ex:
+#         return bad_request("Invalid timestamp ({})".format(str(ex)))
+#
+#     timedelta = request.get('timedelta')
+#     if timedelta is not None and not isinstance(timedelta, int):
+#         return bad_request("Invalid timedelta (Must be an integer)")
+#
+#     repeat = request.get('repeat')
+#     if repeat is not None and not isinstance(repeat, int):
+#         return bad_request("Invalid repeat (Must be an integer)")
+#
+#     if timestamp and timedelta:
+#         return bad_request("timestamp and timedelta are mutually exclusive")
+#
+#     if not timedelta and not timedelta:
+#         return bad_request("one of timestamp and timedelta must be set")
+#
+#     # create new task
+#     task_id = str(uuid.uuid4())
+#     schedule_time = datetime.datetime.now() + datetime.timedelta(seconds=timedelta)
+#     proto_timestamp = timestamp_pb2.Timestamp()
+#     proto_timestamp.FromDateTime(schedule_time)
+#     task = {
+#         'http_request': {
+#             'http_method': 'POST',
+#             'url': os.getenv("EVENT_CALLBACK_URL"),
+#             'oidc_token': {
+#                 'service_account_email': os.getenv('SERVICE_ACCOUNT_EMAIL')
+#             },
+#             'body': {
+#                 'message': message,
+#                 'timedelta': timedelta,
+#                 'id': task_id,
+#                 'repeat': repeat
+#             },
+#             'name': task_id
+#         }
+#     }
+#     task_doc = {
+#         'processed': False,
+#         'schedule_time': schedule_time.isoformat(),
+#         **task,
+#     }
+#     db.collection('events').document(task_id).set(task_doc)
+#     # response = client.create_task(
+#     #     parent=task_queue,
+#     #     task={
+#     #         'schedule_time': proto_timestamp,
+#     #         **task,
+#     #     }
+#     # )
+#     # print(response)
+#     return task_doc, 201
 
 
 def calendar_api(api_request: Request):
